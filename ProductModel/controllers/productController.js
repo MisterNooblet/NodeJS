4c8f59d0514f72fd20819b23dcd5cbe1b46eaee9
misternooblet <dragonroadsupport@gmail.com>
2023-03-22 14:35:52 +0200

    added desc

--- ProductModel/controllers/productController.js
+++ ProductModel/controllers/productController.js
@@ -1,6 +1,11 @@
 import Product from "../models/Product.js";
 import asyncHandler from 'express-async-handler'
 
+// @desc    Get all Products
+// @route   GET /Products
+// @access  Public
+
+
 export const getProducts = asyncHandler(async (req, res) => {
     try {
         const products = await Product.find()
@@ -9,6 +14,10 @@ export const getProducts = asyncHandler(async (req, res) => {
         console.log(error);
     }
 })
+
+// @desc    Delete All Products
+// @route   DELETE /Products
+// @access  Public
 
 export const deleteAllProducts = asyncHandler(async (req, res) => {
     try {
@@ -19,6 +28,9 @@ export const deleteAllProducts = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Get by price range
+// @route   GET /Products/range/:min/:max
+// @access  Public
 
 export const getByPriceRange = asyncHandler(async (req, res) => {
     console.log(typeof Number(req.params.min));
@@ -30,6 +42,11 @@ export const getByPriceRange = asyncHandler(async (req, res) => {
     }
 })
 
+
+// @desc    Get active products
+// @route   GET /Products/active
+// @access  Public
+
 export const getActiveProducts = asyncHandler(async (req, res) => {
     try {
         const products = await Product.find({ isActive: true })
@@ -38,6 +55,10 @@ export const getActiveProducts = asyncHandler(async (req, res) => {
         console.log(error);
     }
 })
+
+// @desc    Add product
+// @route   POST /Products
+// @access  Public
 
 
 export const addProduct = asyncHandler(async (req, res) => {
@@ -51,6 +72,10 @@ export const addProduct = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    DELETE product
+// @route   DELETE /Products/:id
+// @access  Public
+
 export const deleteProduct = asyncHandler(async (req, res) => {
     try {
         const product = await Product.findByIdAndDelete(req.params.id)
@@ -60,6 +85,11 @@ export const deleteProduct = asyncHandler(async (req, res) => {
         res.status(500).send('Server error');
     }
 })
+
+
+// @desc    Update product
+// @route   PUT /Products/:id
+// @access  Public
 
 export const updateProduct = asyncHandler(async (req, res) => {
     try {
@@ -76,6 +106,10 @@ export const updateProduct = asyncHandler(async (req, res) => {
         res.status(500).send('Server error');
     }
 })
+
+// @desc    Get product
+// @route   GET /Products/:id
+// @access  Public
 
 export const getProduct = asyncHandler(async (req, res) => {
     try {
